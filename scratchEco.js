61bbbcd36c08dd5573a7ea088f9577c3c9ef923e
SimaKyr <simaduck@gmail.com>
2018-10-31 16:50:26 +0200

    Create scratchEco.js

---
+++ scratchEco.js
@@ -0,0 +1,275 @@
+
+    var temp = document.createElement("script");
+temp.src = "https://www.gstatic.com/firebasejs/5.5.3/firebase.js";
+temp.onload = function(){connect();};
+document.getElementsByTagName('head')[0].appendChild(temp);
+function connect(){
+    var tempa = document.createElement('script');
+var scriptct=`var config = {
+    apiKey: "hide",
+    authDomain: "hide",
+    databaseURL: "hide",
+    projectId: "hide",
+    storageBucket: "hide",
+    messagingSenderId: "hide"
+  };
+  firebase.initializeApp(config);
+  function set(key,value){firebase.database().ref().child(key).set(value);}
+  firebase.database().ref().on('value', snap => get = snap.val());
+`
+tempa.innerHTML = scriptct;
+document.body.appendChild(tempa);
+}
+document.head.appendChild(temp);
+
+window.onload = function() {
+var script = document.createElement("script");
+var iS = "`";
+var injectAbout = `<div class='btnaboutm'><img src='https://simakyr.github.io/scratchEco/icons/about.png' height='16px'><div><h5>Help</h5><p>You can use for inject image [img]url to image[/img]</p></div></div>
+<style>.btnaboutm{
+height:16px;
+width:16px;
+}
+.btnaboutm div {
+display:none;
+position: absolute;
+height:60px;
+width:250px;
+background-color:grey;
+box-shadow: 0 0 10px rgba(0,0,0,0.5);
+padding:10px;
+z-index: 1;
+color:white;
+text-shadow: none;
+border-radius: 10px;
+    }
+.btnaboutm:hover div {
+    display: inline;
+}
+</style>`
+
+var scriptc=`
+//искать тег [img] [/img] и вырезать из него url на картинку
+function getUrlImgFromMessege(messege){
+var m = messege;
+var i=0;
+var text = [];
+while(m.indexOf('[img]')!=-1){
+var s=m.indexOf('[img]')+5;
+var e=m.indexOf('[/img]');
+text[i] = m.substring(s,e).replace('[img]','');
+var m=m.replace('[img]','').replace('[/img]','').replace(text,'');
+i++;
+}return text;}
+
+
+//сканирует все сообщения и переводит их в нормальные
+function makeReadable(){
+var m = getMesseges();
+var i=0;
+var me;
+while(i!=m.length){
+me=m[i].substring(m[i].indexOf('☁')+1,m[i].replace('☁','').indexOf('☁')+1);
+if(parseInt(me)==me){
+m[i] = get['m'+me];
+}
+i++;
+}
+setMesseges(m);
+}
+
+function addBtnAbout(){
+var html = '<img onclick="openStats()" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAAFiQAABYkBbWid+gAAABl0RVh0U29mdHdhcmUAd3d3Lmlua3NjYXBlLm9yZ5vuPBoAAAGjSURBVEiJ3dW/S5VRGAfwj17vVKQ0hcO17tAvClIbm3SKBgf/gDBwkCZrKReJIgLdnMNosKChKYQgaMnAoZZIyqVAGposbmWh3oZzLvfel/ftvtp18QuHc97z/Pie9z3f53nZYxRa2Iso42j0/d4u4uN4iHVUG8Yn3Eb3/yQfx28s4wpO4ggGcBNfsIbzu0k+hi1cR0eGz0E8wTec3knyEn7hRg7fAp7jDTrzEszhneaLL+IZXuFwwr+MPxjJS7CGa4m9HlSwiVMpMYt4kCd5t6CSCym2Ek5kxN0SxJCJrjgfiPOPhP199PmJcynxFeHSM1G7oK/CZygl7GcFpRzKiC8Jsm1JsIklXErYt6ItK/YiXv6LoBGXBZkea9ibxIxQ0TPoS/hvCG0kFwp4LWi7J+4NY7Rh1KTaL/SlO3mT19CLVXzAUIq9CxPx5BsY3CmBeMrH2MYK7mMWjwQxVOJ6Oz6n1UcunME0nuIFFnBVaHziuioUaHm3JK0wFUk+axZAW3E3knxUf7u2Yy6SvFVXYFvRgflIcm8vCAh1NCH8bvcR/gLcbFO0Hvor8QAAAABJRU5ErkJggg==">';
+document.getElementsByClassName('header-text')[0].getElementsByTagName('h2')[0].innerHTML = a=document.getElementsByClassName('header-text')[0].getElementsByTagName('h2')[0].innerHTML+ html;
+}
+
+function makeReadableM(){
+var m = getMessegesM();
+var i=0;
+var me;
+while(i!=m.length){
+me=m[i].substring(m[i].indexOf('☁')+1,m[i].replace('☁','').indexOf('☁')+1);
+if(parseInt(me)==me){
+m[i] = get['m'+me];
+}
+i++;
+}
+setMessegesM(m);
+}
+
+function sendMessege(m){
+document.getElementsByClassName("control-group tooltip right")[0].getElementsByTagName('textarea')[0].value=m;
+document.getElementsByClassName("control-group")[1].getElementsByClassName("button small")[0].click();
+}
+
+function antiB(i){
+if(i==0){
+//анти баг для кнопки на странице /messeges/
+document.getElementsByClassName("button messages-social-loadmore")[0].setAttribute("onclick","makeReadableM();");
+}
+
+if(i==1){
+//антибаг для кнопки на странице проекта
+var a=document.getElementsByClassName("comments")[0].getElementsByTagName('div');
+a[a.length-1].setAttribute("onclick","makeReadable();");
+}}
+
+//получить текст сообщения
+
+function getText(){
+return document.getElementById('comments').getElementsByTagName('textarea')[0].value
+}
+
+//создать кнопку
+function createBtn(){
+if(document.getElementById('btnSend')==null){
+var para = document.createElement("div");
+para.className = "button small";
+para.id = "btnSend";
+para.innerHTML = '<a onclick="sendCustom()";>Send with ScratchEco</a>'
+document.getElementsByClassName("control-group")[1].appendChild(para);
+}}
+
+//получает никнейм текущего профиля
+function getNickprofile(){
+return document.getElementsByClassName('header-text')[0].getElementsByTagName('h2')[0].innerText
+}
+
+//открывает статистику текущего профиля в новом окне
+function openStats(){
+window.open('https://scratchstats.com/#' + getNickprofile());
+}
+
+//получить текст сообщения(текст храниться в array)
+function getMesseges(){
+var i=0;
+var text = [];
+while(i!=document.getElementsByClassName("comments")[0].getElementsByTagName('li').length){
+text[i]=document.getElementsByClassName("comments")[0].getElementsByTagName('li')[i].getElementsByClassName("content")[0].innerHTML;
+i++;
+}return text;}
+//задать текст(текст храниться в array)
+function setMesseges(text){
+var i=0;
+while(i!=document.getElementsByClassName("comments")[0].getElementsByTagName('li').length){
+document.getElementsByClassName("comments")[0].getElementsByTagName('li')[i].getElementsByClassName("content")[0].innerHTML=text[i];
+i++;
+}}
+//сканирует и добавляет картинку к сообщению где надо + убирает мусор(теги)
+function tagsScan(){
+var i=0;
+var m=getMesseges();
+var temp;
+var para;
+while(i!=m.length){
+img = getUrlImgFromMessege(m[i]);
+if(img.length!=0){
+temp=0;
+while(temp!=img.length){
+para = document.createElement("img");
+para.height = '128';
+para.src = img[temp];
+m[i] = m[i].replace('[img]'+img[temp]+'[/img]', '');
+m[i] = m[i]+para.outerHTML;
+temp++;
+}
+}i++;}setMesseges(m);return m;}
+
+//получить сообщения на странице /messeges/
+function getMessegesM(){
+var i=0;
+var text = [];
+while(i!=document.getElementsByClassName("emoji-text mod-comment").length){
+text[i]=document.getElementsByClassName("emoji-text mod-comment")[i].innerHTML;
+i++;
+}return text;}
+
+//задать сообщения на странице /messeges/
+function setMessegesM(text){
+var i=0;
+while(i!=document.getElementsByClassName("emoji-text mod-comment").length){
+document.getElementsByClassName("emoji-text mod-comment")[i].innerHTML=text[i];
+i++;
+}}
+
+//получаем никнейм создателя проекта
+function getCredit(){return document.getElementById('owner').innerHTML;}
+
+//добавляет кнопку для гиф Trumbnail
+function addBtnGif(){
+var para = document.createElement("div");
+para.className = 'button';
+para.id = 'gifAdd';
+para.setAttribute(onclick,"alert('ok')");
+para.height = "50";
+para.setAttribute('onclick','gifSet();')
+para.innerHTML = "<span>GIF Trumbnail</span>";
+document.getElementsByClassName('buttons')[0].appendChild(para);
+}
+
+//гиф код от World_Languages
+function gifSet(){
+x=document.createElement("script");x.src="//is.gd/thumb2"; document.getElementsByTagName("head")[0].appendChild(x);
+}
+
+//получает никнейм текущего аккуанта
+function getNickname(){
+if(document.getElementById('navigation')==null){return document.getElementsByTagName('span')[2].innerText;}
+else{return document.getElementsByTagName('span')[8].innerText;}}
+
+//добавляет кнопку если нужно
+function addGif(){
+    if(document.getElementById('gifAdd')==null){
+if(getCredit()==getNickname()){addBtnGif();}}
+}
+
+//добавляет кнопку об комментах
+function addBtnhelp(){
+    if(document.getElementsByClassName('btnaboutm')[0]==undefined){
+var a = ` + iS + injectAbout + iS + `;
+document.getElementsByClassName('control-group')[1].innerHTML = document.getElementsByClassName('control-group')[1].innerHTML + a;
+    }
+}
+//отправка для кнопки
+function sendCustom(){
+set('mL',get['mL']+1);
+set('m'+ get['mL'],getText());
+sendMessege('☁'+ get['mL'] +'☁');
+}
+runM();
+function runM(){
+url = document.location.href.split("/").slice(3);
+if(url.includes('users') || url.includes('projects') || url.includes('comments')){
+//antiB(1);
+}
+else{
+//antiB(0);
+makeReadableM();
+}
+
+if(url.includes('users')){addBtnAbout();}
+
+}
+carrot=0;
+setInterval(fixes,1000);
+function fixes(){
+    if(url.includes('users') || url.includes('projects') || url.includes('comments')){
+makeReadable();
+tagsScan();
+createBtn();
+addBtnhelp();
+carrot=getMesseges().lenght;
+if(url.includes('projects')){
+addGif();
+}
+    }
+    else{
+    makeReadableM()
+    carrot=getMessegesM().lenght;
+    }
+}
+`;
+
+script.innerHTML = scriptc;
+document.head.appendChild(script);
+}
