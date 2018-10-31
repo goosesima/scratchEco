9178f6e049c60a969fb7ed3da03e65df3899296f
SimaKyr <simaduck@gmail.com>
2018-10-31 16:57:45 +0200

    Create scratchEco.user.js

---
+++ scratchEco.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         ScratchEco
+// @namespace    http://scratcheco.cf/
+// @version      0.0
+// @description  Scratch Eco - it is extension for Scratch, what make site better.
+// @author       SimaKyr
+// @match        scratch.mit.edu/*
+// @grant        none
+// ==/UserScript==
+var temp = document.createElement("script");temp.src="https://simakyr.github.io/scratchEco/scatchEco-min.js";document.head.appendChild(temp);
