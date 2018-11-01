8a5f9c56e5f36f7c282a76f8857dabc4f8285c14
SimaKyr <simaduck@gmail.com>
2018-11-01 19:04:10 +0200

    Update scratchEco.js

--- scratchEco.js
+++ scratchEco.js
@@ -85,15 +85,14 @@ function setCordInfoFrame(elem){
 document.getElementById("infoFrame").style.left = getOffset(elem).left + 'px';
 document.getElementById("infoFrame").style.top = getOffset(elem).top + 'px';
 }
-
 function changePlayer(){
-var num = parseInt(window.location.href.split('#')[0].replace(/\D+/g,""));
+
 if('null' == localStorage.getItem('playerScratch')){
 localStorage.setItem('playerScratch', '0');
 }
 player = localStorage.getItem('playerScratch');
 if(player != 0){
-
+var num = parseInt(window.location.href.split('#')[0].replace(/\D+/g,""));
     //Create element html
     var html = '<iframe height=400 width=480 frameborder="0" style="position:absolute;top:50%;left:50%;margin:-200px 0 0 -250px;" '
         if(player==1){
