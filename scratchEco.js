d8ba766197d4d6310763fe3e0a49d81c5ce63bf3
SimaKyr <simaduck@gmail.com>
2018-11-01 20:20:33 +0200

    Update scratchEco.js

--- scratchEco.js
+++ scratchEco.js
@@ -92,7 +92,6 @@ localStorage.setItem('playerScratch', '0');
 }
 player = localStorage.getItem('playerScratch');
 if(player != 0){
-idproject = document.documentURI.split('#')[0].replace(/\D+/g,"");
     //Create element html
     var html = '<iframe height=400 width=480 frameborder="0" style="position:absolute;top:50%;left:50%;margin:-200px 0 0 -250px;" '
         if(player==1){
@@ -121,6 +120,7 @@ function addChangerPlayer(){
 var html = '<select id="selectorPlayer" style="width:100px;"><option value="0">Original</option><option value="1">Phosphorus</option><option value="2">Surforus</option></select>';
 var para = document.createElement('div')
 para.innerHTML = html;
+idproject = document.location.href.split("/").slice(3)[1];
 document.getElementById('stats').appendChild(para);
 var val = localStorage.getItem('playerScratch');    
 document.querySelector('#selectorPlayer [value="' + val + '"]').selected = true;
