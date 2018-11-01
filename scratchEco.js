71c8a5f6190df07c21186e0bb7526edee81a24ec
SimaKyr <simaduck@gmail.com>
2018-11-01 20:06:33 +0200

    Update scratchEco.js

--- scratchEco.js
+++ scratchEco.js
@@ -1,7 +1,6 @@
 
 var temp = document.createElement("script");
 temp.src = "https://www.gstatic.com/firebasejs/5.5.3/firebase.js";
-temp.onload = function(){connect();};
 document.getElementsByTagName('head')[0].appendChild(temp);
 function connect(){
 var config = {
@@ -93,7 +92,7 @@ localStorage.setItem('playerScratch', '0');
 }
 player = localStorage.getItem('playerScratch');
 if(player != 0){
-idproject = parseInt(window.location.href.split('#')[0].replace(/\D+/g,""));
+idproject = parseInt(document.documentURI.split('#')[0].replace(/\D+/g,""));
     //Create element html
     var html = '<iframe height=400 width=480 frameborder="0" style="position:absolute;top:50%;left:50%;margin:-200px 0 0 -250px;" '
         if(player==1){
