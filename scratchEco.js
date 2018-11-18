8973c97fba860f1db16e73a82755ddead4e5ca42
SimaKyr <simaduck@gmail.com>
2018-11-18 17:39:18 +0200

    Update scratchEco.js

--- scratchEco.js
+++ scratchEco.js
@@ -67,7 +67,10 @@ function getOffset(el) {
 }
 
 function isElementToProfile(el){
+    if(el.href.length>2){
 return el.href.split("/").slice(3)[1] == el.innerText;
+    }
+    else{return false;}
 }
 
 function infoFrameCreate(){
@@ -96,12 +99,12 @@ var eleme = 'player';
 if(typeof codeOriginalPlayer == 'undefined'){codeOriginalPlayer = document.getElementById(eleme).innerHTML;}
 
     //Create element html
-    var html = '<iframe height=400 width=480 frameborder="0" style="position:absolute;top:50%;left:50%;margin:-200px 0 0 -250px;" '
+    var html = '<iframe allowfullscreen height=400 width=480 frameborder="0" style="position:absolute;top:50%;left:50%;margin:-200px 0 0 -250px;" '
         if(player==1){
             html = html + 'src=https://phosphorus.github.io/embed.html?id=' + idproject + '&turbo=false&full-screen=false></iframe>';
         }
         if(player==2){
-            html = html + 'src=https://sulfurous.aau.at/html/app.html?id=' + idproject + '&turbo=false&full-screen=false></iframe>';
+            html = html + 'src=https://sulfurous.aau.at/versions/v0.90/html/embed.html?id=' + idproject + '&turbo=false&full-screen=false></iframe>';
         }
         if(player==0){
             html=codeOriginalPlayer;
